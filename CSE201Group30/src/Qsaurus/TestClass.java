c4dd80f260a31fcd6d890da8ad34af38c6e7a783
Administrator <Administrator@CHAP104CS01>
2018-03-24 13:02:07 -0400

    Took out Team member println statement

--- CSE201Group30/src/Qsaurus/TestClass.java
+++ CSE201Group30/src/Qsaurus/TestClass.java
@@ -4,9 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This was added by Project Lead");
-		System.out.println("This was added by Project Member");
-		
+		System.out.println("This was added by Project Lead");		
 	}
 
 }
